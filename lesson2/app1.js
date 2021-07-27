eb860d786886ff2b5417b0a20e905fc59cc6c849
Hieuln99 <87683551+Hieuln99@users.noreply.github.com>
2021-07-27 12:49:28 +0700

    t

--- lesson2/app1.js
+++ lesson2/app1.js
@@ -3,17 +3,19 @@ const app = express()
 const {getCurrentDate,printLog} = require('./hai')
 
 app.use(express.urlencoded({extended:true}))
-
+app.set('view engine'.'hbs')
 
 app.post('/survey',(req,res)=>{
     var nameInput = req.body.txtName;
-    res.setHeader('Content-Type','text/html');
+    var job =req.body.job;
+    res.render('survey',{name:nameInput, job:job})
    // res.end('<h1>Thank you ' + nameInput +'</h1>')
-   res.write(`<h1>Thank you ${namInput} for ...<h1>`)
+   res.write(``)
    var now = getCurrentDate();
    printLog(now)
    res.end(`Current date: ${now}`)
 })
+
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + "/views/home.html")
