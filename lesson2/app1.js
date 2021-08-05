d3274cedea07333a53bf9d51d91d0a426db60e31
Hieuln99 <87683551+Hieuln99@users.noreply.github.com>
2021-08-05 07:51:30 +0700

    k

--- lesson2/app1.js
+++ lesson2/app1.js
@@ -1,29 +1,25 @@
-const express = require ('express')
+const express = require('express')
 const app = express()
-const {getCurrentDate,printLog} = require('./hai')
+const {hetCurrentDate, printLog, getCurrentDate} = require('./hai')
 
-app.use(express.urlencoded({extended:true}))
-app.set('view engine'.'hbs')
+app.use(express.urlencoded({extend:true}))
 
 app.post('/survey',(req,res)=>{
-    var nameInput = req.body.txtName;
-    var job =req.body.job;
-    res.render('survey',{name:nameInput, job:job})
-   // res.end('<h1>Thank you ' + nameInput +'</h1>')
-   res.write(``)
-   var now = getCurrentDate();
-   printLog(now)
-   res.end(`Current date: ${now}`)
-})
-
-
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + "/views/home.html")
-})
-app.get('/about', (req,res)=>{
+    var nameInput =req.body.txtName;
+    var jobInput = req.body.job;
     res.setHeader('Content-Type','text/html');
-    res.end('<h1 style="color:red"> About page </h1>')
+    res.write(`<h1>Thanks you ${nameInput} - ${jobInput} for participating <h1>`)
+    var now =getCurrentDate();
+    printLog(now)
+    res.end(`Current date: ${now}`)
+})
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/views/home.html')
+})
+app.get('/about',(req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    res.end('<h1 style: "color:red">About page</h1>')
 })
 const PORT = 5000;
 app.listen(PORT);
-console.log('Server is running on Port: ', PORT)
+console.log('Server is running: ', PORT)
